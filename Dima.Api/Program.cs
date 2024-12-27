df0d839d9181f0fceafcf3308ea0a756dc9ee7c8
Rafael Vieira Suarez <rafaelv_s@hotmail.com>
2024-12-27 18:56:19 -0300

    feat: Criação dos endpoints  handlers e requests e responses

--- Dima.Api/Program.cs
+++ Dima.Api/Program.cs
@@ -1,6 +1,22 @@
+using Dima.Api;
+using Dima.Api.Common.Api;
+using Dima.Api.Endpoints;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.AddConfiguration();
+builder.AddSecurity();
+builder.AddDataContexts();
+builder.AddCrossOrigin();
+builder.AddDocumentation();
+builder.AddServices();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+    app.ConfigureDevEnvironment();
 
-app.Run();
+app.UseCors(ApiConfiguration.CorsPolicyName);
+app.UseSecurity();
+app.MapEndpoints();
+
+await app.RunAsync();
